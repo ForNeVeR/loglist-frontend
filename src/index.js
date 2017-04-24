e560ed38d5ddcb826daa86ef98bd307c38c93221
vyalkin <sergey.Vyalkin@enterra-inc.com>
2017-04-24 18:39:29 +0700

    initial commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import Hello from './components/Hello';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+    <Hello />,
+    document.getElementById('root')
+);
---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'public'),
+        filename: 'bundle.js'
+    },
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                query: {
+                    presets: ['es2015', 'react']
+                }
+            },
+            {
+                test: /\.css$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: 'style-loader',
+                    },
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            importLoaders: 1,
+                        }
+                    },
+                    {
+                        loader: 'postcss-loader'
+                    }
+                ]
+            }
+        ]
+    }
+}
